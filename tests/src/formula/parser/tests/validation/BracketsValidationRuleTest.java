4be3bfde9b3b49b8f16dfd4d56f412d8261abe6b
IvannKurchenko <ivankoorchenko@gmail.com>
2013-09-08 15:29:28 +0300

    BracketValidationRuleTest implemented

--- tests/src/formula/parser/tests/validation/BracketsValidationRuleTest.java
+++ tests/src/formula/parser/tests/validation/BracketsValidationRuleTest.java
@@ -1,4 +1,35 @@
 package formula.parser.tests.validation;
 
+import formula.parser.FormulaParseException;
+import formula.parser.token.FormulaTokenizer;
+import formula.parser.validation.FormulaValidationException;
+import formula.parser.validation.FormulaValidationRules;
+import org.junit.Test;
+
 public class BracketsValidationRuleTest {
+
+    @Test
+    public void shouldPassValidation() throws FormulaParseException {
+        checkValidation("() () (( )) ");
+    }
+
+    @Test(expected = FormulaValidationException.class)
+    public void shouldFailValidationThroughBracketWrongPosition() throws FormulaParseException {
+        checkValidation("() )(");
+    }
+
+    @Test(expected = FormulaValidationException.class)
+    public void shouldFailValidationThroughCloseBracketsCountLessThenOpen() throws FormulaParseException {
+        checkValidation("() ((( ))");
+    }
+
+    @Test(expected = FormulaValidationException.class)
+    public void shouldFailValidationThroughOpenBracketsCountLessThenClose() throws FormulaParseException {
+        checkValidation("() (( )))");
+    }
+
+    private void checkValidation(String testString) throws FormulaParseException {
+        FormulaTokenizer tokenizer = new FormulaTokenizer(testString);
+        FormulaValidationRules.BRACKETS_RULE.validate(tokenizer);
+    }
 }
