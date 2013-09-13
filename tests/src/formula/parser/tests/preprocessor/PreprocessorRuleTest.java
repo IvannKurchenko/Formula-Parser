5b60178163e1f3cc1e85e574b77fc91124fe05f5
IvannKurchenko <ivankoorchenko@gmail.com>
2013-09-13 15:18:18 +0300

    Minus problem fixing, second step : MinusPreprocessorRuleTest created

--- tests/src/formula/parser/tests/preprocessor/PreprocessorRuleTest.java
+++ tests/src/formula/parser/tests/preprocessor/PreprocessorRuleTest.java
@@ -1,9 +1,13 @@
 package formula.parser.tests.preprocessor;
 
+import formula.parser.FormulaParseException;
 import formula.parser.preprocessor.FormulaPreprocessorRule;
 import formula.parser.token.FormulaToken;
+import formula.parser.token.FormulaTokenizer;
 
 import java.util.List;
+
+import static formula.parser.tests.util.AssetUtil.assertListEquals;
 
 public class PreprocessorRuleTest {
 
@@ -13,8 +17,15 @@ public class PreprocessorRuleTest {
         this.testFormulaPreprocessorRule = testFormulaPreprocessorRule;
     }
 
+    protected void assertPrepossessResult(String incomingFormula, String expectedFormula) throws FormulaParseException {
+        List<FormulaToken> incomingTokenList = splitOnTokens(incomingFormula);
+        testFormulaPreprocessorRule.prepossess(incomingTokenList);
+        List<FormulaToken> expectedTokenList = splitOnTokens(expectedFormula);
+        assertListEquals(incomingTokenList, expectedTokenList);
 
-    protected void prepossess(List<FormulaToken> tokenList){
-        testFormulaPreprocessorRule.prepossess(tokenList);
+    }
+
+    private List<FormulaToken> splitOnTokens(String formulaString) throws FormulaParseException {
+        return new FormulaTokenizer(formulaString).getTokenList();
     }
 }
