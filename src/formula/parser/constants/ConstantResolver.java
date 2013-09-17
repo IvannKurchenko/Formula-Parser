ab6b09e5e4c0f6342bf6884110900553c403d159
IvannKurchenko <ivankoorchenko@gmail.com>
2013-09-17 23:42:06 +0300

    Javadoc added to ConstantResolver

--- src/formula/parser/constants/ConstantResolver.java
+++ src/formula/parser/constants/ConstantResolver.java
@@ -4,6 +4,9 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Util class for adding and searching of constants.
+ */
 public class ConstantResolver {
 
     private static final Map<String, Double> CONSTANTS_SIGN_VALUE_MAP = createdConstantsSignValueMap(Constants.values());
@@ -18,6 +21,11 @@ public class ConstantResolver {
         return Collections.unmodifiableMap(signValueMap);
     }
 
+    /**
+     * Return supported constants by default.
+     *
+     * @return map of supported constants by default, where key -sign of constant, value - constant value.
+     */
     public static Map<String, Double> getSupportedConstants() {
         return CONSTANTS_SIGN_VALUE_MAP;
     }
@@ -29,18 +37,35 @@ public class ConstantResolver {
         constantsSignValueMap = new HashMap<String, Double>(CONSTANTS_SIGN_VALUE_MAP);
     }
 
-    public void addConstant(String constantName, Double constantValue) {
-        constantsSignValueMap.put(constantName, constantValue);
+    /**
+     * Add custom constant to known constants.
+     *
+     * @param constantSign  constant sign, same as in formula string should be.
+     * @param constantValue constant value.
+     */
+    public void addConstant(String constantSign, Double constantValue) {
+        constantsSignValueMap.put(constantSign, constantValue);
         constantMaxLength = -1;
     }
 
+    /**
+     * Return maximum length of known constants.
+     *
+     * @return maximum length of known constants.
+     */
     public int getConstantsMaxLength() {
-        if(constantMaxLength < 0 ){
+        if (constantMaxLength < 0) {
             findConstantMaxLength();
         }
         return constantMaxLength;
     }
 
+    /**
+     * Search constant by given sign.
+     *
+     * @param sign sign of constant to search.
+     * @return value of found constant.
+     */
     public Double findConstantBySign(String sign) {
         return constantsSignValueMap.get(sign);
     }
